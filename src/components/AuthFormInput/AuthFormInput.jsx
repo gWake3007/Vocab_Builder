dd92ac21a40765ce27b08f1609c2b0e3c51fdb41
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-01-31 22:11:13 +0100

    123

---
+++ src/components/AuthFormInput/AuthFormInput.jsx
@@ -0,0 +1,9 @@
+import { ErrorMessage, Field } from 'formik';
+import { useId, useState } from 'react';
+import style from './AuthFormInput.module.css';
+
+const AuthFormInput = () => {
+  return <div>AuthFormInput</div>;
+};
+
+export default AuthFormInput;
