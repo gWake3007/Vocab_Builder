d75b7ec3b0f6a89378e5b8b056ed0e0931d6c199
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-02-02 22:00:21 +0100

    123

--- src/components/AuthFormInput/AuthFormInput.jsx
+++ src/components/AuthFormInput/AuthFormInput.jsx
@@ -2,8 +2,41 @@ import { ErrorMessage, Field } from 'formik';
 import { useId, useState } from 'react';
 import style from './AuthFormInput.module.css';
 
-const AuthFormInput = () => {
-  return <div>AuthFormInput</div>;
+const AuthFormInput = ({ errors, touched, placeholder, type }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const id = useId();
+
+  const getTypeForInput = () => {
+    if (type === 'password') {
+      return showPassword ? 'text' : 'password';
+    } else return type;
+  };
+
+  return (
+    <div className={style.wrap}>
+      <label className={style.label} htmlFor={`${type}${id}`}>
+        {placeholder}
+      </label>
+      <Field
+        className={`${
+          errors.password && touched.password ? style.inputError : ''
+        } ${style.input}`}
+        type={getTypeForInput()}
+        name={type}
+        placeholder={placeholder}
+        id={`${type}${id}`}
+      />
+      <ErrorMessage className={style.error} name={type} component="span" />
+      {getTypeForInput() === 'password' && (
+        <button
+          type="button"
+          className={style.btnHide}
+          onClick={() => setShowPassword(!showPassword)}
+        ></button>
+      )}
+    </div>
+  );
 };
 
 export default AuthFormInput;
