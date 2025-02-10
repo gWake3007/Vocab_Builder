66ea175af0e924291cbb41acd94da1dc337f0798
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-02-10 09:40:03 +0100

    123

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,3 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import vocabBuilderApi from '../../api/api.js';
+import toast from 'react-hot-toast';
+
+axios.defaults.baseURL = 'https://vocab-builder-backend.p.goit.global';
+
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+let hasShownToast = false;
