d20cdba792fcf942a6b757777f85e19ddd35c7d4
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2025-02-17 23:12:39 +0100

    123

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,11 +1,17 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import toast from 'react-hot-toast';
+import { persistor } from '../store.js';
 
 axios.defaults.baseURL = 'https://vocab-builder-backend.p.goit.global';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const cleanHeaderToken = () => {
+  axios.defaults.headers.common.Authorization = '';
+  persistor.purge();
 };
 
 let hasShownToast = false;
